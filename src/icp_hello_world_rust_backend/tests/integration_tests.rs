c552959d9d2ff88291d694272c58163b45f2a949
Elias Datler <eliasdatler@me.com>
2024-01-22 21:26:59 +0000

    cargo fmt

--- src/icp_hello_world_rust_backend/tests/integration_tests.rs
+++ src/icp_hello_world_rust_backend/tests/integration_tests.rs
@@ -1,6 +1,6 @@
-use std::fs;
-use candid::{Principal, encode_one, decode_one};
+use candid::{decode_one, encode_one, Principal};
 use pocket_ic::{PocketIc, WasmResult};
+use std::fs;
 
 const BACKEND_WASM: &str = "../../icp_hello_world_rust_backend.wasm";
 
@@ -8,7 +8,7 @@ fn setup() -> (PocketIc, Principal) {
     let pic = PocketIc::new();
 
     let backend_canister = pic.create_canister();
-    pic.add_cycles(backend_canister, 2_000_000_000_000); // 2 Tcycles
+    pic.add_cycles(backend_canister, 2_000_000_000_000); // 2T Cycles
     let wasm = fs::read(BACKEND_WASM).expect("Build the wasm with ./scripts/build.sh");
     pic.install_canister(backend_canister, wasm, vec![], None);
     (pic, backend_canister)
@@ -18,10 +18,14 @@ fn setup() -> (PocketIc, Principal) {
 fn test_hello_world() {
     let (pic, backend_canister) = setup();
 
-    let Ok(WasmResult::Reply(response)) = pic.query_call(backend_canister, Principal::anonymous(), "greet", encode_one("ICP").unwrap()) else {
+    let Ok(WasmResult::Reply(response)) = pic.query_call(
+        backend_canister,
+        Principal::anonymous(),
+        "greet",
+        encode_one("ICP").unwrap(),
+    ) else {
         panic!("Expected reply");
     };
-
     let result: String = decode_one(&response).unwrap();
     assert_eq!(result, "Hello, ICP!");
 }
