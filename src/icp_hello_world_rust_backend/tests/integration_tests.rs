da2e7cfd73dd4376a98e89db5f4f53e2421b23b3
Elias Datler <46360620+fxgst@users.noreply.github.com>
2024-02-02 10:00:38 +0100

    fix(VER-2656,VER-2663): Fix candid metadata not being supplied (#2)
    - Fixes an issue where the candid UI canister could not display the
    canister's interface because the custom build script did not include the
    candid metadata field. `dfx deploy` will now use the default build
    script by `dfx` which includes the necessary metadata
    - Bumps the dfx version such that the candid ui canister now works in
    all scenarios
    - Extends the `canister_url` script to feature the link to the candid ui
    canister

--- src/icp_hello_world_rust_backend/tests/integration_tests.rs
+++ src/icp_hello_world_rust_backend/tests/integration_tests.rs
@@ -2,14 +2,14 @@ use candid::{decode_one, encode_one, Principal};
 use pocket_ic::{PocketIc, WasmResult};
 use std::fs;
 
-const BACKEND_WASM: &str = "../../icp_hello_world_rust_backend.wasm";
+const BACKEND_WASM: &str = "../../target/wasm32-unknown-unknown/release/icp_hello_world_rust_backend.wasm";
 
 fn setup() -> (PocketIc, Principal) {
     let pic = PocketIc::new();
 
     let backend_canister = pic.create_canister();
     pic.add_cycles(backend_canister, 2_000_000_000_000); // 2T Cycles
-    let wasm = fs::read(BACKEND_WASM).expect("Build the wasm with ./scripts/build.sh");
+    let wasm = fs::read(BACKEND_WASM).expect("Wasm file not found, run 'dfx build'.");
     pic.install_canister(backend_canister, wasm, vec![], None);
     (pic, backend_canister)
 }
